babe14871939278159bdab2ccad393466345649e
Marcelo Núñez Bustos <marcelo1nunez@gmail.com>
2018-06-18 21:51:16 -0400

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/cafe'
 }else{
-    urlDB = 'mongodb://cafe-user:123456a@ds163300.mlab.com:63300/cafe_mar';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
